bf681a3e5a9dc3dfc7b2e29c6868c9d012d70f70
IvanS297 <ivan.ser202@gmail.com>
2026-05-17 22:47:21 +0700

    Added Frontier exploration package and frontier explporation custom messages

---
+++ frontier_exploration/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'frontier_exploration'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ivan',
+    maintainer_email='ivan.ser202@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    entry_points={
+        'console_scripts': [
+            "frontier_exploration=frontier_exploration.frontier_exploration:main",
+            "pure_persuit=frontier_exploration.pure_persuit:main",
+        ],
+    },
+)
